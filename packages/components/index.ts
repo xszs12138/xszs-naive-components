2581eb8a80522567dc52d11a0e7fcef6857d448a
xszs <2457331233@qq.com>
2026-03-31 17:29:22 +0800

    chore: 优化组件包文件位置

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,11 +1,11 @@
+// export all components
+export { default as XForm } from './@core/XForm/index.vue'
+
+export { default as XFormCollection } from './@core/XFormCollection/index.vue'
+// export all types
+export type { XFormSchema } from './@core/XFormCollection/types'
+
 // export all hooks
 export { default as useTable } from './hooks/useTable'
 
 export { default as dealSpanMethod } from './utils/dealSpanMethod'
-// export all components
-export { default as XForm } from './XForm/index.vue'
-
-export { default as XFormCollection } from './XFormCollection/index.vue'
-
-// export all types
-export type { XFormSchema } from './XFormCollection/types'
